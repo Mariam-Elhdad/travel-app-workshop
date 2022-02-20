feddb031fa76fd6f8e104440a124069ecd906fb8
mariam.elhdad5@gmail.com <mariam.elhdad5@gmail.com>
2022-02-20 11:37:31 +0200

    first commit

---
+++ home/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def Home(request):
+    return(HttpResponse('<h1> Its home page </h1>'))
+    
+
+def Profile(request):
+    return(HttpResponse('<h1> Its your profile page </h1>'))
+
+def Registration(request):
+    return(HttpResponse('<h1> Its your registration page </h1>'))   
