0fc0c2211011c33f70380617f97829e58ebedd0f
MariamZayed <mariamzayed91@gmail.com>
2022-02-24 02:46:29 +0200

    Add admin user
    Create Vehicle model\table
    
    added admin user, so now you can access the admin page. Also set the
    first table in the DB.

--- home/models.py
+++ home/models.py
@@ -1,4 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
-# class (models.Model):
-    
+class Vehicle(models.Model):
+    vehicle_name = models.CharField(max_length=30)
+    model_name = models.CharField(max_length=30)
+    production_year = models.CharField(max_length=50)
+    available_seat = models.IntegerField()
+    desc = models.TextField()
+    vehicle_color = models.CharField(max_length=30)
+    license_plate = models.IntegerField() 
+    created_at = models.DateTimeField(default=timezone.now)
+
+    #this means when you call query Vehicle.objects.all() it will appear by its vehicle_name not 'Vehicle objects (1)'\
+    # You need to run the shell again to see the result
+    def __str__(self): 
+        return self.vehicle_name
